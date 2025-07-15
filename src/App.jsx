73f5f78a8129dcedcc44aac787137cfd37463b65
Jia Li <chin.j3li@gmail.com>
2025-07-15 10:59:56 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [display, setDisplay] = useState(true);
+  if (display === true) {
+    return (
+      <>
+        <div className="container mt-3 card">
+          <div className="card-body">
+            <h2>Newsletter Subscription</h2>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+              }}
+            >
+              <div class="mb-3">
+                {""}
+                <input
+                  id="name"
+                  type="text"
+                  class="form-control"
+                  placeholder="Your Name"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  id="myemail"
+                  type="email"
+                  class="form-control"
+                  placeholder="Your Email"
+                  required
+                />
+              </div>
+              <button
+                className="btn btn-sm btn-primary"
+                onClick={() => {
+                  setEmail(myemail.value);
+                  setDisplay(false);
+                }}
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="container mt-5">
+          <div
+            class="alert alert-success"
+            role="alert"
+            style={{ display: "block" }}
+          >
+            Please check your email ({email}) for confirmation.
+          </div>
+          <a
+            className="text-primary link-underline"
+            onClick={() => {
+              setDisplay(true);
+            }}
+          >
+            Subscribe to another email
+          </a>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
